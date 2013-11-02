0a01582c6e2675433aa7fd964ddc1abc70a66b79
fahrulazmi <fahrulazmi@froztbytes.com>
2013-11-02 15:04:26 +0800

    Create UIDeviceHardware.h

---
+++ UIDeviceHardware.h
@@ -0,0 +1,10 @@
+//
+//  UIDeviceHardware.h
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIDeviceHardware : NSObject
+    + (NSString *) platform;
+    + (NSString *) platformString;
+@end
