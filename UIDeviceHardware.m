697e44691f2f8f1af574030504d604e2fa916c3f
fahrulazmi <fahrulazmi@froztbytes.com>
2013-11-28 11:30:46 +0800

    Fixed #1 typo for iPad Mini Retina (CDMA)

--- UIDeviceHardware.m
+++ UIDeviceHardware.m
@@ -59,7 +59,7 @@
     if ([platform isEqualToString:@"iPad4,1"])      return @"iPad Air (WiFi)";
     if ([platform isEqualToString:@"iPad4,2"])      return @"iPad Air (CDMA)";
     if ([platform isEqualToString:@"iPad4,4"])      return @"iPad Mini Retina (WiFi)";
-    if ([platform isEqualToString:@"iPad4,4"])      return @"iPad Mini Retina (CDMA)";
+    if ([platform isEqualToString:@"iPad4,5"])      return @"iPad Mini Retina (CDMA)";
     
     if ([platform isEqualToString:@"i386"])         return @"Simulator";
     if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
