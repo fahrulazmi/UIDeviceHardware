084abaf21468bf446c94eaa6c60bbee7a3b9ecec
Michael Wybrow <mjwybrow@samediff.net>
2015-09-25 00:17:57 +0800

    Clean up device names.

--- UIDeviceHardware.m
+++ UIDeviceHardware.m
@@ -31,13 +31,13 @@
     if ([platform isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
     if ([platform isEqualToString:@"iPhone5,1"])    return @"iPhone 5 (GSM)";
     if ([platform isEqualToString:@"iPhone5,2"])    return @"iPhone 5 (CDMA)";
-    if ([platform isEqualToString:@"iPhone5,3"])    return @"iPhone 5C";
-    if ([platform isEqualToString:@"iPhone5,4"])    return @"iPhone 5C";
-    if ([platform isEqualToString:@"iPhone6,1"])    return @"iPhone 5S";
-    if ([platform isEqualToString:@"iPhone6,2"])    return @"iPhone 5S";
+    if ([platform isEqualToString:@"iPhone5,3"])    return @"iPhone 5c";
+    if ([platform isEqualToString:@"iPhone5,4"])    return @"iPhone 5c";
+    if ([platform isEqualToString:@"iPhone6,1"])    return @"iPhone 5s";
+    if ([platform isEqualToString:@"iPhone6,2"])    return @"iPhone 5s";
     if ([platform isEqualToString:@"iPhone7,1"])    return @"iPhone 6 Plus";
     if ([platform isEqualToString:@"iPhone7,2"])    return @"iPhone 6";
-    
+
     if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch 1G";
     if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch 2G";
     if ([platform isEqualToString:@"iPod3,1"])      return @"iPod Touch 3G";
@@ -59,20 +59,17 @@
     if ([platform isEqualToString:@"iPad3,4"])      return @"iPad 4 (WiFi)";
     if ([platform isEqualToString:@"iPad3,5"])      return @"iPad 4 (GSM)";
     if ([platform isEqualToString:@"iPad3,6"])      return @"iPad 4 (CDMA)";
-    
     if ([platform isEqualToString:@"iPad4,1"])      return @"iPad Air (WiFi)";
     if ([platform isEqualToString:@"iPad4,2"])      return @"iPad Air (GSM)";
     if ([platform isEqualToString:@"iPad4,3"])      return @"iPad Air (CDMA)";
-    if ([platform isEqualToString:@"iPad5,3"])      return @"iPad Air 2 (WiFi)";
-    if ([platform isEqualToString:@"iPad5,4"])      return @"iPad Air 2 (CDMA)";
-    
     if ([platform isEqualToString:@"iPad4,4"])      return @"iPad Mini Retina (WiFi)";
-    if ([platform isEqualToString:@"iPad4,5"])      return @"iPad Mini Retina (CDMA)";
+    if ([platform isEqualToString:@"iPad4,5"])      return @"iPad Mini Retina (Cellular)";
     if ([platform isEqualToString:@"iPad4,7"])      return @"iPad Mini 3 (WiFi)";
-    if ([platform isEqualToString:@"iPad4,8"])      return @"iPad Mini 3 (CDMA)";
-    if ([platform isEqualToString:@"iPad4,9"])      return @"iPad Mini 3 (CDMA)";
-    
-    
+    if ([platform isEqualToString:@"iPad4,8"])      return @"iPad Mini 3 (Cellular)";
+    if ([platform isEqualToString:@"iPad4,9"])      return @"iPad Mini 3 (Cellular)";
+    if ([platform isEqualToString:@"iPad5,3"])      return @"iPad Air 2 (WiFi)";
+    if ([platform isEqualToString:@"iPad5,4"])      return @"iPad Air 2 (Cellular)";
+
     if ([platform isEqualToString:@"i386"])         return [UIDevice currentDevice].model;
     if ([platform isEqualToString:@"x86_64"])       return [UIDevice currentDevice].model;
     
